a26298bc3f8300160519448be559e905d42ee084
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2010-09-16 10:56:33 +0200

    patch to 'embedd' by Jonathan Shore
    now handles multivariate time series too.

--- tseriesChaos/R/embedd.R
+++ tseriesChaos/R/embedd.R
@@ -1,15 +1,26 @@
-#Author: Antonio, Fabio Di Narzo. Last Modified $Date: 2005-12-02 17:15:39 +0100 (ven, 02 dic 2005) $
-embedd <- function(x, m, d, lags) {
-	x <- as.ts(x)
-	if(missing(lags)) {
-		checkEmbParms(x, m, d)
-		lags <- ((1:m)-1)*d
-	}
-	res <- lag(x, lags[1])
-	for(i in 2:length(lags)) {
-		res <- ts.intersect(res, lag(x, lags[i]))
-	}
-	res <- matrix(res, nr = nrow(res), nc = ncol(res))
-	colnames(res) <- paste("lag", lags, sep="")
-	res
+embedd <- function (x, m, d, lags) {
+  if (is.null(colnames(x))) {
+    vars <- sprintf("V%d", 1:NCOL(x))
+  } else {
+    vars <- colnames(x)
+  }
+
+  x <- as.ts(x)
+  if (missing(lags)) {
+    checkEmbParms(x, m, d)
+    lags <- ((1:m) - 1) * d
+  }
+
+  names <- sprintf("%s/%d", vars, lags[1])
+  res <- lag(x, lags[1])
+  if(length(lags) > 1) {
+    for (i in 2:length(lags)) {
+      res <- ts.intersect(res, lag(x, lags[i]))
+      names <- append(names, sprintf("%s/%d", vars, lags[i]))
+    }
+  }
+
+  res <- matrix(res, nr = nrow(res), nc = ncol(res))
+  colnames(res) <- names
+  res
 }
